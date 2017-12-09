d97b66e62f96edf4af6ca9d3ffbe583bbe393cf2
u014427391 <3623307211@qq.com>
2017-12-09 19:07:54 +0800

    添加树形菜单工具类

---
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/utils/MenuTreeUtil.java
@@ -0,0 +1,49 @@
+package org.muses.jeeplatform.utils;
+
+
+import org.muses.jeeplatform.core.entity.admin.Menu;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuTreeUtil {
+
+    public List<Menu> menuCommon;
+    public List<Menu> list = new ArrayList<Menu>();
+      
+    public List<Menu> menuList(List<Menu> menus){
+        this.menuCommon = menus;
+        for (Menu x : menus) {
+            Menu m = new Menu();
+            if(x.getParentId()==0){
+                m.setParentId(x.getParentId());
+                m.setMenuId(x.getMenuId());
+                m.setMenuName(x.getMenuName());
+                m.setMenuIcon(x.getMenuIcon());
+                m.setMenuUrl(x.getMenuUrl());
+                m.setSubMenu(menuChild(x.getMenuId()));
+                list.add(m);
+            }  
+        }     
+        return list;  
+    }  
+   
+    public List<Menu> menuChild(int id){
+        List<Menu> lists = new ArrayList<Menu>();
+        for(Menu a:menuCommon){
+            Menu m = new Menu();
+            if(a.getParentId() == id){
+                m.setParentId(a.getParentId());
+                m.setMenuId(a.getMenuId());
+                m.setMenuName(a.getMenuName());
+                m.setMenuIcon(a.getMenuIcon());
+                m.setMenuUrl(a.getMenuUrl());
+                lists.add(m);
+            }  
+        }  
+        return lists; 
+    }  
+
+}
