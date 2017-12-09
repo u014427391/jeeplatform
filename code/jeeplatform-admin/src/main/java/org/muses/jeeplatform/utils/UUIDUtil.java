79bae414e9d14e0f0deb98f5b033c2ecf33baa89
u014427391 <3623307211@qq.com>
2017-12-09 19:08:51 +0800

    添加UUID工具类

---
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/utils/UUIDUtil.java
@@ -0,0 +1,15 @@
+package org.muses.jeeplatform.utils;
+
+import java.util.UUID;
+
+/**
+ * Created by Nicky on 2017/11/19.
+ */
+public class UUIDUtil {
+
+    public static String getRandomNum(){
+        String s = UUID.randomUUID().toString();
+        s =  s.substring(0,8)+s.substring(9,13)+s.substring(14,18)+s.substring(19,23)+s.substring(24);
+        return s.substring(0, 12);
+    }
+}
