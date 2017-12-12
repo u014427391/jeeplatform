730e4b2dddc658fd8d38d0fbcbdcecb033db8cf2
nicky <362330721@qq.com>
2017-12-12 14:36:10 +0800

    元注解查询缓存的接口

--- code/jeeplatform-common/src/main/java/org.muses/jeeplatform.common/annotation/QueryCache.java
+++ code/jeeplatform-common/src/main/java/org.muses/jeeplatform.common/annotation/QueryCache.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.CacheNamespace;
 import java.lang.annotation.*;
 
 /**
- * 元注解申明查询缓存的接口，设置Retention在JVM里
+ * 元注解申明查询缓存的接口，通过Retention设置Annotations将被JVM保存
  */
 @Documented
 @Target(ElementType.METHOD)
