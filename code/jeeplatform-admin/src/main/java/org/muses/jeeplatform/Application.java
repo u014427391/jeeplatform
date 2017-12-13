4ace4bfb7fb30308d10b475eb5764bf1005c4332
Nicky <362330721@qq.com>
2017-12-13 16:04:10 +0800

    添加Redis缓存Spring AOP实现

--- code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/Application.java
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/Application.java
@@ -1,15 +1,26 @@
 package org.muses.jeeplatform;
 
+
+import org.muses.jeeplatform.cache.RedisClient;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author caiyuyu
@@ -21,10 +32,31 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 //@EnableCaching
 @EnableAsync
-
+@RestController
 public class Application {
+
+    @Autowired
+    RedisClient redisClient;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @RequestMapping("/set")
+    public String set(String key, String value) throws Exception{
+        redisClient.setValue(key, value);
+        return "success";
+    }
+
+    @RequestMapping("/get")
+    public String get(String key) throws Exception {
+        return redisClient.getValue(key);
+    }
+//    @Bean
+//    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+//        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
+//        c.setIgnoreUnresolvablePlaceholders(true);
+//        return c;
+//    }
+
 }
