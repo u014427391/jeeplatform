d8dcb55592595166cba52f4a2ea59ea6f76c73be
nicky <362330721@qq.com>
2017-12-21 16:49:54 +0800

    同一账号同时登录人数控制

--- code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/config/ShiroConfig.java
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/config/ShiroConfig.java
@@ -4,6 +4,7 @@ import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.muses.jeeplatform.core.shiro.ShiroRealm;
+import org.muses.jeeplatform.web.filter.SysAccessControllerFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -50,5 +51,26 @@ public class ShiroConfig {
         DefaultWebSecurityManager securityManager =  new DefaultWebSecurityManager();
         securityManager.setRealm(myShiroRealm());
         return securityManager;
+    }
+
+    /**
+     * 限制同一账号登录同时登录人数控制
+     * @return
+     */
+    public SysAccessControllerFilter kickoutSessionControlFilter(){
+        SysAccessControllerFilter filter = new SysAccessControllerFilter();
+        //使用cacheManager获取相应的cache来缓存用户登录的会话；用于保存用户—会话之间的关系的；
+        //这里我们还是用之前shiro使用的redisManager()实现的cacheManager()缓存管理
+        //也可以重新另写一个，重新配置缓存时间之类的自定义缓存属性
+//        filter.setCacheManager(cacheManager());
+//        //用于根据会话ID，获取会话进行踢出操作的；
+//        filter.setSessionManager(sessionManager());
+        //是否踢出后来登录的，默认是false；即后者登录的用户踢出前者登录的用户；踢出顺序。
+        filter.setKickoutAfter(false);
+        //同一个用户最大的会话数，默认1；比如2的意思是同一个用户允许最多同时两个人登录；
+        filter.setMaxSession(1);
+        //被踢出后重定向到的地址；
+        filter.setUrl("/login");
+        return filter;
     }
 }
