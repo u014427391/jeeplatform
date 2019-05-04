ba307e887cefb78daf786c435b45f5f68d0f8045
smileNicky <3623307211@qq.com>
2019-05-04 16:04:54 +0800

    feat：编辑权限信息功能

--- code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/web/controller/PermissionController.java
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/web/controller/PermissionController.java
@@ -1,20 +1,26 @@
 package org.muses.jeeplatform.web.controller;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.muses.jeeplatform.core.Constants;
 import org.muses.jeeplatform.core.entity.admin.Permission;
 import org.muses.jeeplatform.service.PermissionPageService;
+import org.muses.jeeplatform.service.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Nicky on 2017/12/3.
@@ -25,6 +31,8 @@ public class PermissionController extends BaseController {
 
     @Autowired
     PermissionPageService permissionPageService;
+    @Autowired
+    PermissionService permissionService;
 
     /**
      * 查询所有权限信息
@@ -59,5 +67,55 @@ public class PermissionController extends BaseController {
         return mv;
     }
 
+    /**
+     * 跳转到编辑权限信息页面
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/goEditP", method = RequestMethod.GET)
+    public String goEditP(@RequestParam("pId")String pId, Model model){
+        Permission permission = this.permissionService.getOne(Integer.parseInt(pId));
+        model.addAttribute("permission" , permission);
+        return "admin/permission/permission_edit";
+    }
+
+    /**
+     * 编辑权限信息
+     * @param params
+     */
+    @RequestMapping(value = "/editP", method = RequestMethod.POST)
+    @ResponseBody
+    public void editR(@RequestParam("params")String params, HttpServletResponse response){
+        String strs[]=params.split(",");
+        String id = strs[0];
+        String name = strs[1];
+        String pdesc = strs[2];
+        Permission permission = new Permission();
+        permission.setId(Integer.parseInt(id));
+        permission.setName(name);
+        permission.setPdesc(pdesc);
+
+        PrintWriter out = null;
+
+        response.setCharacterEncoding("utf-8");
+
+        JSONObject obj = new JSONObject();
+
+        try {
+            out = response.getWriter();
+            permissionService.doSave(permission);
+            obj.put("result","success");
+            out.write(obj.toString());
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            obj.put("result","error");
+            out.write(obj.toString());
+            out.flush();
+            out.close();
+        }
+
+    }
 
 }
