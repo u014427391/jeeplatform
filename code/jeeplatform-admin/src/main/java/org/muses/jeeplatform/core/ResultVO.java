d3e8adc4896e0263a190b6c1b1f39db14e181013
Nicky.Ma <3623307211@qq.com>
2019-05-12 20:21:07 +0800

    feat:SpringBoot集成lombok

--- code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/core/ResultVO.java
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/core/ResultVO.java
@@ -1,8 +1,11 @@
 package org.muses.jeeplatform.core;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 /**
  * <pre>
- *
+ *  接口返回类
  * </pre>
  *
  * @author nicky
@@ -11,5 +14,42 @@ package org.muses.jeeplatform.core;
  *    修改后版本:     修改人：  修改日期: 2019年05月12日  修改内容:
  * </pre>
  */
-public class ResultVO {
+
+@Data
+public class ResultVO<T> {
+
+    @ApiModelProperty("状态码")
+    private Integer status;
+    @ApiModelProperty("返回信息")
+    private String message;
+    @ApiModelProperty("返回数据")
+    private T data;
+
+    public ResultVO(Integer status, String message){
+        this.status = status;
+        this.message = message;
+    }
+
+    public ResultVO(Integer status, String message, T data){
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ResultVO error(String message) {
+        return new ResultVO(0, message, null);
+    }
+
+    public static <T> ResultVO error(String message,T data) {
+        return new ResultVO(0, message, data);
+    }
+
+    public static <T> ResultVO successful(String message){
+        return new ResultVO(1,message,null);
+    }
+
+    public static <T> ResultVO successful(String message, T data){
+        return new ResultVO(1, message, data);
+    }
+
 }
