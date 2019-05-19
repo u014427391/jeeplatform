16de250fc453e18618880f79b6db478e089be214
Nicky.Ma <3623307211@qq.com>
2019-05-19 14:42:57 +0800

    feat：客户端先暂时接入CAS单点登录

---
+++ code/jeeplatform-admin/src/main/java/org/muses/jeeplatform/web/filter/CASFilter.java
@@ -0,0 +1,20 @@
+package org.muses.jeeplatform.web.filter;
+
+
+import org.jasig.cas.client.authentication.AuthenticationFilter;
+
+/**
+ * <pre>
+ *  CAS认证Filter
+ * </pre>
+ *
+ * @author nicky
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2019年05月19日  修改内容:
+ * </pre>
+ */
+public class CASFilter extends AuthenticationFilter {
+
+
+}
