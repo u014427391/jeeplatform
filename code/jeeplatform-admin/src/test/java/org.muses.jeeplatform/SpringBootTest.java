83e32edd85b434a4a7ff449e5d951d326def86d4
Nicky.Ma <3623307211@qq.com>
2019-11-02 20:37:19 +0800

    添加SpringBoot Junit测试

---
+++ code/jeeplatform-admin/src/test/java/org.muses.jeeplatform/SpringBootTest.java
@@ -0,0 +1,27 @@
+package org.muses.jeeplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <pre>
+ *     SpringBoot Junit测试类
+ * </pre>
+ *
+ * @author nicky
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2019年11月02日  修改内容:
+ * </pre>
+ */
+@RunWith(SpringRunner.class)
+@org.springframework.boot.test.context.SpringBootTest
+public class SpringBootTest {
+
+    @Test
+    public void testConfigurationProperties(){
+
+    }
+
+}
