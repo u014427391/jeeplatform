74949cfbf3234c877b156d4b0348af8cac684877
马增群 <mazq@minstone.com.cn>
2019-11-08 18:17:45 +0800

    test:测试@PropertySource注解使用

--- code/jeeplatform-admin/src/test/java/org.muses.jeeplatform/bean/User.java
+++ code/jeeplatform-admin/src/test/java/org.muses.jeeplatform/bean/User.java
@@ -1,6 +1,7 @@
 package org.muses.jeeplatform.bean;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -19,6 +20,7 @@ import java.util.Map;
  * </pre>
  */
 @Component
+@PropertySource("classpath:user.properties")
 @ConfigurationProperties(prefix = "user")
 public class User {
 
