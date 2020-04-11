c87c5d8658acd5124c5e39917f5938ff2dc155c4
Nicky.Ma <3623307211@qq.com>
2020-04-11 20:52:04 +0800

    提交OA工程

---
+++ code/jeeplatform-oa/src/main/java/org/muses/jeeplatform/oa/OAApplication.java
@@ -0,0 +1,14 @@
+package org.muses.jeeplatform.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAApplication.class, args);
+    }
+
+
+}
